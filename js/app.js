8f3094d5ae994ce94d540b851101ca9ca565d54d
Corneliousm808 <corneliousmooring@gmail.com>
2017-02-03 09:20:44 -1000

    no back

---
+++ js/app.js
@@ -0,0 +1,9 @@
+function card(id) {
+  document.getElementById(id).style.transform= 'rotateY(180deg)';
+
+  if(document.getElementById(id).style.transform === 'rotateY(180deg)'){
+    setTimeout(function(){
+    document.getElementById(id).style.transform = 'rotateY(0deg)';
+    },1000);
+  }
+}
